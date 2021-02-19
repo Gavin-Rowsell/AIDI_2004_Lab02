e43b037fb7e9fa93c464649dcafb9a2e1b918da6
Gavin Rowsell <gavin.rowsell@dcmail.ca>
2021-02-19 03:31:24 -0500

    Fixed whitespace

--- basicMath.py
+++ basicMath.py
@@ -1,7 +1,7 @@
 var1 = input("Enter a number: ")
 var2 = input("Enter another number: ")
 var3 = int(var1) + int(var2)
-print("Number 1 plus number 2 = ", var3)
+print("Number 1 plus number 2 =", var3)
 
 
 
